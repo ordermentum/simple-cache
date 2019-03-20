cf1c11ec4c0d33e61ff65910780023b7b3c93910
danielmoi <danielmoi@users.noreply.github.com>
2019-03-20 15:53:09 +1100

    Add types (#2)
    * Add types
    
    * Add the other types

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+import { RedisClient } from 'redis';
+
+declare function OrdermentumCacheMachine(
+  redis: RedisClient
+): {
+  redis: RedisClient;
+
+  parse: (val: string) => object | false;
+
+  set: (key: string, value: any, ttl?: number) => Promise<any>;
+
+  get: (key: string, defaultValue: any | null) => Promise<any>;
+
+  getOrSet: (key: string, fallback: () => any, ttl?: number) => Promise<any>;
+
+  getAndDel: (key: string) => Promise<any>;
+
+  rateLimit: (key: string, limit?: number, window?: number) => Promise<boolean>;
+};
+
+export = OrdermentumCacheMachine;
