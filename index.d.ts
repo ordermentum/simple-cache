8c2617ae23af69087d897c9d152eca23a9f92d4a
Daniel Moi <dmoixy@gmail.com>
2019-03-20 15:23:34 +1100

    Add the other types

--- index.d.ts
+++ index.d.ts
@@ -5,11 +5,17 @@ declare function OrdermentumCacheMachine(
 ): {
   redis: RedisClient;
 
-  set: (key: string, value: any, ttl?: number) => string;
+  parse: (val: string) => object | false;
 
-  get: (key: string) => any;
+  set: (key: string, value: any, ttl?: number) => Promise<any>;
 
-  getAndDel: (id: string) => any;
+  get: (key: string, defaultValue: any | null) => Promise<any>;
+
+  getOrSet: (key: string, fallback: () => any, ttl?: number) => Promise<any>;
+
+  getAndDel: (key: string) => Promise<any>;
+
+  rateLimit: (key: string, limit?: number, window?: number) => Promise<boolean>;
 };
 
 export = OrdermentumCacheMachine;
